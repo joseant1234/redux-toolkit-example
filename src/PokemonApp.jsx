2baad656fc21e3b93d27b424e8311e692a38084d
Jose <jose.antonio.umeres@gmail.com>
2022-06-25 22:07:55 -0500

    Added pagination to pokemons

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from './store/slices/pokemon/thunks';
 
 export const PokemonApp = () => {
 
   const dispatch = useDispatch();
+  const { isLoading, pokemons = [], page } = useSelector(state => state.pokemons);
 
   // solo carga al iniciar el componente
   useEffect(() => {
@@ -15,9 +16,21 @@ export const PokemonApp = () => {
     <>
         <h1>PokemonApp</h1>
         <hr />
+        <span>Loading: { isLoading ? 'True' : 'False' }</span>
         <ul>
-            <li></li>
+          {
+            pokemons.map(({ name }) => (
+              <li key={name}>{ name }</li>
+            ))
+          }
         </ul>
+
+        <button
+          disabled={isLoading}
+          onClick={() => dispatch(getPokemons(page)) }
+        >
+          Next
+        </button>
     </>
   )
 }
--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -6,9 +6,9 @@ export const getPokemons = (page = 0) => {
         // con el getState se tiene acceso al state de la app
         dispatch(startLoadingPokemons());
         // realizar peticion http
-        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1&offset=${page * 10}`);
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
         // const data = await resp.json();
-        const { data } = await pokemonApi.get(`/pokemon?limit=1&offset=${page * 10}`);
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${page * 10}`);
 
         dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
     }
