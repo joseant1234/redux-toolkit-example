6ef7fca118784f3e8e2b7160805a8ac0d91ad559
Jose <jose.antonio.umeres@gmail.com>
2022-06-26 11:53:27 -0500

    Added RTK Query

---
+++ src/TodoApp.jsx
@@ -0,0 +1,17 @@
+
+
+export const TodoApp = () => {
+  return (
+    <>
+        <h1>Todos - RTK Query</h1>
+        <hr/>
+        <h4>isLoading...</h4>
+
+        <pre>...</pre>
+
+        <button>
+            Next Todo
+        </button>
+    </>
+  )
+}
---
+++ src/store/apis/todosApi.js
@@ -0,0 +1,21 @@
+import  { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+// baseQuery para establecer una configuracion base
+// query: () => '/todos' concatena a lo establecido en baseUrl
+export const todosApi = createApi({
+    reducerPath: 'todos',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://jsonplaceholder.typicode.com'
+    }),
+    endpoints: (builder) => ({
+        getTodos: builder.query({
+            query: () => '/todos'
+        })
+    })
+
+})
+
+// createApi crea principalmente hooks
+// le pone query para saber q es un get, si fuera un post se pondria como sufijo una mutation
+// pone el prefijo 'use' por el estandar de react para los hooks
+export const { useGetTodosQuery } = todosApi;
